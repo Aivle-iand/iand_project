f1a0a2e906a74175725352ed4941da746daeb222
namho <namhogo@naver.com>
2023-12-21 17:17:08 +0900

    v1.13

--- community/views.py
+++ community/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def announcement(request):
+    return render(request, 'announcement.html')
+
+def freeboard(request):
+    return render(request, 'freeboard.html')
+
+def qna(request):
+    return render(request, 'qna.html')
