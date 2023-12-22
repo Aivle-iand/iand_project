0372353e21eaaa48ab3ae31ad9d1e1683f8e3ef1
seongjiko <soc06202@naver.com>
2023-12-22 11:16:01 +0900

    urls수정

--- iand_project/settings.py
+++ iand_project/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login',
     'main',
     'playground',
     'community',
