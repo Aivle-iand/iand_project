9d9f6210be6fc440011799387f7dbf5e6ae7b665
namho <namhogo@naver.com>
2023-12-23 01:02:30 +0900

    커뮤니티 글쓰기 기능

--- community/models.py
+++ community/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
 # Create your models here.
+# 게시글(Post)엔 제목(postname), 내용(contents)이 존재합니다
+class Post(models.Model):
+    postname = models.CharField(max_length=50)
+    contents = models.TextField()
+
+    # 게시글의 제목(postname)이 Post object 대신하기
+    def __str__(self):
+        return self.postname
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.CharField(max_length=20)
+    message = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- iand_project/settings.py
+++ iand_project/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     'playground',
     'community',
     'mypage',
+    'login',
 ]
 
 MIDDLEWARE = [
