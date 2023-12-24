8651130cadbba80e582e650287a0ef3d8e9a4de4
namho <namhogo@naver.com>
2023-12-24 12:28:10 +0900

    게시판글쓰기및게시판

--- community/models.py
+++ community/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 # Create your models here.
 # 게시글(Post)엔 제목(postname), 내용(contents)이 존재합니다
-class Post(models.Model):
-    postname = models.CharField(max_length=50)
+class Board(models.Model):
+    postname = models.CharField(max_length=64, verbose_name="제목")
     contents = models.TextField()
+    registered_date = models.DateTimeField(auto_now_add=True, verbose_name="등록 시간")
+    writer = models.ForeignKey('users.Users', verbose_name="글쓴이", on_delete=models.CASCADE)
 
     # 게시글의 제목(postname)이 Post object 대신하기
     def __str__(self):
         return self.postname
+    
+    class Meta:
+        db_table = "community_board"
+        verbose_name = "게시물"
+        verbose_name_plural = "게시물"
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -15,3 +22,4 @@ class Comment(models.Model):
     message = models.TextField()
     created = models.DateField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
--- community/views.py
+++ community/views.py
@@ -6,32 +6,42 @@ from django.http import HttpResponse
 
 
 def writepage(request):
-    if request.method == 'POST':
-        new_article=Post.objects.create(
-            postname=request.POST['postname'],
-            contents=request.POST['contents'],
+    if not request.session.get('user'):
+        return redirect('../login')
+    if request.method == 'GET':
+        form = PostForm()
+        
+    elif request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            user_id = request.session.get('user')
+            user = Users.objects.get(pk=user_id)
+            
+            new_article=Board(
+                postname = form.cleaned_data['postname'],
+                contents = form.cleaned_data['contents'],
+                writer = user
             )
         new_article.save()
         
-        return redirect(f'community:posting',new_article.id)
+        return redirect(f'community:posting', new_article.id)
     return render(request, 'writepage.html')
 
 # Create your views here.
 def announcement(request):
-    post_list = Post.objects.all()
+    post_list = Board.objects.all()
     return render(request, 'announcement.html', {'post_all':post_list})
 
 def freeboard(request):
-    post_list = Post.objects.all()
+    post_list = Board.objects.all().order_by('-id')
     return render(request, 'freeboard.html', {'post_all':post_list})
 
 def qna(request):
-    post_list = Post.objects.all()
+    post_list = Board.objects.all()
     return render(request, 'qna.html', {'post_all':post_list})
 
 def posting(request, pk):
     # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
-    post = Post.objects.get(id=pk)
+    post = Board.objects.get(id=pk)
     # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
     return render(request, 'posting.html', {'posting':post})
-
