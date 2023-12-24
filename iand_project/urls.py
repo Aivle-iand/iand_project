a7b32a38506dc7b7ff6218a08550000c14f41b01
seongjiko <soc06202@naver.com>
2023-12-24 13:17:43 +0900

    demo main

--- iand_project/urls.py
+++ iand_project/urls.py
@@ -20,12 +20,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('login.urls')),
+    path('accounts/', include('allauth.urls')),
     path('playground/', include('playground.urls')),
     path('community/', include('community.urls')),
     path('mypage/', include('mypage.urls')),
-    path('accounts/', include('login.urls')),
-    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
