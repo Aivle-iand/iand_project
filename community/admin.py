5880d0139461fef3b58cc757f25210f223131b96
namho <namhogo@naver.com>
2023-12-27 12:55:49 +0900

    temp

--- community/admin.py
+++ community/admin.py
@@ -7,10 +7,8 @@ from .models import *
 # 관리자(admin)가 게시글(Post)에 접근 가능
 # class BoardAdmin(admin.ModelAdmin):
 #     list_display = ('postname',)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
 
-
-admin.site.register(BoardAnn)
-
-admin.site.register(BoardFree)
-
-admin.site.register(BoardQna)
+admin.site.register(Board)
+admin.site.register(Category, CategoryAdmin)
--- community/urls.py
+++ community/urls.py
@@ -2,16 +2,21 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
-from community.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'community'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.announcement, name='announcement'),
-    path('freeboard', views.freeboard, name='freeboard'),
-    path('qna', views.qna, name='qna'),
+    path('', views.categoryView, name='post_all'),
+    # path("<str:category_name>/", views.categoryView, name="category",)
+    # path('freeboard', views.freeboard, name='freeboard'),
+    # path('qna', views.qna, name='qna'),
     path('writepage', views.writepage, name='writepage'),
-    path('ann/<int:pk>',views.posting_ann, name='posting_ann'),
-    path('free/<int:pk>',views.posting_free, name='posting_free'),
-    path('qna/<int:pk>',views.posting_qna, name='posting_qna'),
+    # path('', views.announcement, name='announcement'),
+    path('<slug:c_slug>/', views.categoryView, name='post_by_category'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
