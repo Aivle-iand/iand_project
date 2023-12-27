38ec7e78944c99cd1f4fbdf7a41b340796f7855e
sujung <tnwjd318@gmail.com>
2023-12-27 14:17:15 +0900

    에피소드 model 추가 및 view 함수 3가지 사항 수정

--- playground/views.py
+++ playground/views.py
@@ -3,9 +3,8 @@ from .models import Book
 
 
 def index(request):
-    search_query = request.GET.get('search', '')
-    filter_option = request.GET.get('filter', '')
-    categoryOption = request.GET.get('categoryOption', '')
+    search_query, filter_option, categoryOption = request.GET.get('search', ''), request.GET.get('filter', ''), request.GET.get('categoryOption', '')
+    voice, face = request.GET.get('voice', ''), request.GET.get('face', '')
     
     books = Book.objects.all()
 
@@ -16,9 +15,7 @@ def index(request):
         
     if search_query:
         books = books.filter(name__icontains=search_query)
-        
-        
-        
+                
     if categoryOption:
         if categoryOption == '0':  # '전체' 선택 시 모든 책 보여주기
             pass  # 모든 책을 이미 불러왔으므로 추가 조건 없음
@@ -26,7 +23,16 @@ def index(request):
             cate = {'1':'과학자', '2':'수학자', '3': '철학자', '4':'음악가'}
             books = books.filter(category=cate[categoryOption])
 
-    return render(request, 'playground/lol.html', {'books': books, "search_query": search_query, "filter_option" : filter_option, "categoryOption":categoryOption })
+    if voice:
+        pass
+        # 여기서 음성 api 써야 합니다. 
+    if face:
+        pass
+        # 여기서 얼굴 합성 api 써야 합니다. 
+        # ex) reading_book = books.filter(id=face)
+        # ex) api 코드 
+
+    return render(request, 'playground/lol.html', {'books': books, "search_query": search_query, "filter_option" : filter_option, "categoryOption":categoryOption, "voice":voice, "face":face})
 
 
 
@@ -35,3 +41,18 @@ def index(request):
 def profile_view(request):
     profiles = Book.objects.all()
     return render(request, 'playground/lol.html', {'profiles': profiles})
+
+
+
+
+# episode 불러오는 view 함수 추가
+from django.shortcuts import render, get_object_or_404
+
+def book_detail(request, book_id, episode_number=None):
+    book = get_object_or_404(Book, pk=book_id)
+    if episode_number:
+        episodes = book.episodes.filter(episode_number=episode_number)
+    else:
+        episodes = book.episodes.all()
+
+    return render(request, 'playground/lol.html', {'book': book, 'episodes': episodes})
