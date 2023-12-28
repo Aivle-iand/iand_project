543a62a2d13423184e4aaa1a39fa61a0c135b99f
김준우 <junwooha1@gmail.com>
2023-12-28 13:47:50 +0900

    'mypage_add_mainpage'

--- mypage/views.py
+++ mypage/views.py
@@ -5,10 +5,4 @@ def index(request):
     return render(request, 'mypage/mypage_certify.html')
 
 def mypage_temp(request):
-    return render(request, 'mypage/mypage_temp.html')
-
-def upload_img(request):
-    return render(request, 'mypage/face_register.html')
-
-def upload_voice(request):
-    return render(request, 'mypage/voice_register.html')    
+    return render(request, 'mypage/mypage_temp.html') 
