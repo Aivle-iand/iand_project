e96dfed5d05ed55fdf0b6652fef72530a8b1f0a3
namho <namhogo@naver.com>
2023-12-29 17:37:44 +0900

    글쓰기 수정 기능

--- community/urls.py
+++ community/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('writepage/', views.writepage, name='writepage'),
     path('<slug:c_slug>/', views.categoryView, name='post_by_category'),
     path('detail/<int:pk>/', views.detail, name='detail'),
-    path('detail/<int:pk>/update', views.up_post, name='update'),
+    path('update/<int:pk>/', views.update, name='update'),
 ]
 
 if settings.DEBUG:
