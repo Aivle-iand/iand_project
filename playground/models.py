5f4c6ad94fca8a33933422baa49aee17ac2d76ef
seongjiko <soc06202@naver.com>
2023-12-30 13:15:44 +0900

    playground model 구조 변경

--- playground/models.py
+++ playground/models.py
@@ -22,9 +22,14 @@ class Episodes(models.Model):
     title = models.CharField(max_length=30 , default=None,help_text="에피소드 이름")
     image = models.ImageField(upload_to='contents/', default=None, help_text="에피소드 장면 이미지")
     voice = models.FileField(upload_to='voices/',default=None, help_text="기본음성")
-    voice_text = models.CharField(max_length=30,default=None, help_text="음성 대사 텍스트")
-    quiz_text = models.CharField(max_length=100,default=None, help_text="퀴즈 문제 내용")
-    quiz_answer =models.CharField(max_length=30,default=None, help_text="퀴즈 문제 정답")
+    voice_text = models.CharField(max_length=100,default=None, help_text="음성 대사 텍스트")
+    
         
     def __str__(self):
         return f"{self.book.name}_{self.episode_number}_{self.scene_number}"
+
+class Quiz(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='quizzes')
+    quiz_index = models.IntegerField(default=None)
+    quiz_text = models.CharField(max_length=1000,default=None, help_text="퀴즈 문제 내용")
+    quiz_answer =models.CharField(max_length=100,default=None, help_text="퀴즈 문제 정답")
