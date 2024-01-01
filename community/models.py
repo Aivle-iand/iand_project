63c74361113bea7971692de6d43dc957bd4d4334
Gichang404 <gichang404@gmail.com>
2024-01-02 02:58:50 +0900

    import error fixed

--- community/models.py
+++ community/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from django.conf import settings
 # from login.models import User
 
 # Create your models here.
