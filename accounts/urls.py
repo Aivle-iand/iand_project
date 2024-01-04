041849f9b36cbf40d1f19ea07e164b1e9fc60a30
arang <liebe_125@naver.com>
2024-01-04 13:35:07 +0900

    닉네임체크경로수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path("social/signup/", CustomSocialSignupView.as_view(), name="social_signup"),
     path('signup/', SignupView.as_view(), name="account_signup"),
     path('signup/check_username_dup', views.check_username, name="check_username_dup"),
-    path('social/signup/social_check_username_dup', views.social_check_username, name="social_check_username_dup"),
     path('signup/check_nickname_dup', views.check_nickname, name="check_nickname_dup"),
     path('social/signup/social_check_nickname_dup', views.social_check_nickname, name="social_check_nickname_dup"),
     path('', include('allauth.urls')),
