5b80531970d6d8a58e1e7b86efe6c0b9da63f51d
sujung <tnwjd318@gmail.com>
2024-01-04 10:39:25 +0900

    DB table 수정

--- playground/models.py
+++ playground/models.py
@@ -5,6 +5,9 @@ class Book(models.Model):
     name = models.CharField(max_length=90)
     category = models.TextField(null=True, blank=True)
     card_image = models.ImageField(upload_to='books/')  # 'media/books/'에 이미지 저장
+    Ep_title_1 = models.CharField(max_length=30 , default=None,help_text="에피소드1")
+    Ep_title_2 = models.CharField(max_length=30 , default=None,help_text="에피소드2")
+    Ep_title_3 = models.CharField(max_length=30 , default=None,help_text="에피소드3")
     profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/')
     prologue = models.TextField(null=True, blank=True)
     year_of_life = models.TextField(null=True, blank=True)
@@ -19,7 +22,6 @@ class Episodes(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='episodes')
     episode_number = models.PositiveIntegerField(default=1, help_text="에피소드 번호")
     scene_number = models.PositiveIntegerField(default=1, help_text="장면 번호")
-    title = models.CharField(max_length=30 , default=None,help_text="에피소드 이름")
     image = models.ImageField(upload_to='contents/', default=None, help_text="에피소드 장면 이미지")
     voice = models.FileField(upload_to='voices/',default=None, help_text="기본음성")
     voice_text = models.CharField(max_length=100,default=None, help_text="음성 대사 텍스트")
@@ -32,6 +34,7 @@ class Episodes(models.Model):
         
     def __str__(self):
         return f"{self.book.name}_{self.episode_number}_{self.scene_number}"
+
 
 class Quiz(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='quizzes')
