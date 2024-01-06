f63b232e1a531356f5dfb9564697059526ab39e3
arang <liebe_125@naver.com>
2024-01-06 20:45:37 +0900

    로그인오류

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,19 +17,9 @@ class CustomSocialSignupForm(SocialSignup):
         self.fields['username'].widget = forms.HiddenInput()
         self.fields['email'].widget = forms.HiddenInput()
     def save(self, request):
-        # user.additional_field = self.cleaned_data['additional_field']
         user = super(CustomSocialSignupForm, self).save(request)
         return user    
     
-    # def save(self, request):
-    #     user = super(CustomSocialSignupForm, self).save(request)
-    #     user.first_name = self.cleaned_data.get('first_name')
-    #     user.last_name = self.cleaned_data.get('last_name')
-    #     user.nickname = self.cleaned_data.get('nickname')
-    #     user.save()
-    #     return user
-
-
 
 class UserForm(forms.ModelForm):
     class Meta:
@@ -40,13 +30,4 @@ class UserForm(forms.ModelForm):
         super(UserForm, self).__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].required = False
-                        
-# class CustomSignupForm(SignupForm):
-#     first_name = forms.CharField(required=False)
-#     last_name = forms.CharField(required=False)
-#     nickname = forms.CharField(required=False)
-    
-#     def save(self, request):
-#         user = super(CustomSignupForm, self).save(request)
-#         return user
-    
+
