cd1fb53563ea19b8b49b6571b2ee8822bd5914cf
namho <namhogo@naver.com>
2024-01-08 17:08:24 +0900

    dfdfd

--- community/templatetags/custom_filter.py
+++ community/templatetags/custom_filter.py
@@ -6,10 +6,13 @@ register = template.Library()
 
 @register.filter(name='custom_date_format')
 def custom_date_format(value):
+    now = timezone.localtime(value, timezone=timezone.get_default_timezone())
 
-    if isinstance:
-        now = timezone.localtime(value, timezone=timezone.get_default_timezone())
-        # now = now + timezone.timedelta(minutes=1)
-        return now.strftime('%H:%M')
+    if isinstance(value, timezone.datetime):
+
+        # 해당 일자에 작성한 글인 경우
+        if value.day == now.day:
+            return  now.strftime('%H:%M')
+    
     else:
         return date(value, "Y-m-d H:i")
