8f81c2626fa8ad850ef4c0bc62f2036ddf06a541
namho <namhogo@naver.com>
2024-01-08 15:38:37 +0900

    final

--- community/templatetags/custom_filter.py
+++ community/templatetags/custom_filter.py
@@ -6,10 +6,10 @@ register = template.Library()
 
 @register.filter(name='custom_date_format')
 def custom_date_format(value):
-    now = timezone.localtime(value, timezone=timezone.pytz.timezone('Asia/Seoul'))
 
     if isinstance:
-        now = timezone.localtime(value, timezone=timezone.pytz.timezone('Asia/Seoul'))
+        now = timezone.localtime(value, timezone=timezone.get_default_timezone())
+        # now = now + timezone.timedelta(minutes=1)
         return now.strftime('%H:%M')
     else:
         return date(value, "Y-m-d H:i")
